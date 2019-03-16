3c9c717ac37eeff0387f3c7b5ad7521c83549dc0
kevin <cellkevin@qq.com>
2019-03-16 17:06:45 +0800

    !

---
+++ OnlineTest/EightChapter.h
@@ -0,0 +1,73 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<vector>
+
+struct PersonInfo {
+	std::string name;
+	std::vector<std::string> phones;
+};
+
+void Func_8_10(std::string fileName)
+{
+	std::ifstream input(fileName);
+	
+	std::vector<std::string> vec;
+
+	if(input){
+		std::string buf;
+
+		while (std::getline(input, buf))
+		{
+			vec.push_back(buf);
+		}
+	}
+	
+	for (int i = 0; i < vec.size(); ++i)
+	{
+		std::istringstream sin(vec[i]);
+
+		std::string word;
+		while (sin >> word)
+		{
+			std::cout << word << std::endl;
+		}
+	}
+	
+}
+
+
+void Func_8_11()
+{
+	std::string line, word;
+	std::vector<PersonInfo> people;
+	std::istringstream record;
+	while (std::getline(std::cin, line))
+	{
+		PersonInfo info;
+		record.clear();
+		record.str(line);
+		record >> info.name;
+		while (record >> word)
+		{
+			info.phones.push_back(word);
+		}
+		people.push_back(info);
+	}
+}
+
+
+void Func_8_13()
+{
+
+	vector<PersonInfo> people;
+	for (const auto &entry : people)
+	{
+		std::ostringstream formatted, badNums;
+		for (const auto &nums : entry.phones)
+		{
+			//if(valid(nums))
+		}
+	}
+}
--- OnlineTest/main.cpp
+++ OnlineTest/main.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 #include<vector>
-#include <stdio.h>
 #include<cmath>
 #include<iomanip>
 #include<algorithm>
+#include<fstream>
+#include<sstream>
+
+#include"EightChapter.h"
 using namespace std;
 
 class Base1 {
@@ -45,8 +48,51 @@ void fun(Base1*ptr)
 	ptr->display();
 }
 
+istream& myStreamLearn(istream& i)
+{
+	string mm;
+	while (i >> mm)
+	{
+		cout << mm << endl;
+	}
+	i.clear();
+	return i;
+}
 
-int main()
+
+
+
+
+void multiargv(initializer_list<string> il)
+{
+	for(auto beg =il.begin();beg!=il.end();++beg)
+	{
+		cout << *beg << " ";
+	}
+	cout << endl;
+}
+
+
+void PrintArgv(int argc,char* argv[])
+{
+	string str;
+	for (int i = 1; i < argc; ++i)
+	{
+		str += argv[i];
+		str += " ";
+	}
+	cout << str <<" "<<" function version"<< endl;
+}
+
+void OpenFileAndOutPut(char*argv1,char*argv2)
+{
+	ifstream input(argv1);
+	ofstream output(argv2);
+
+
+}
+
+int main(int argc,char*argv[])
 {
 	/*Base1 base1(1);
 	Base2 base2(2);
@@ -55,8 +101,73 @@ int main()
 	fun(&base1);
 	fun(&base2);
 	fun(&base3);*/
-	Derived obj(1, 2, 3, 4);
+	//Derived obj(1, 2, 3, 4);
 
+	///==========================================
+	string fileName ="x.txt";
+
+	//Func_8_10(fileName);
+
+	Func_8_11();
+	//ifstream input(fileName);
+	//vector<string> vec;
+	//if (input)
+	//{
+	//	string buf;
+	//	/*while (getline(input,buf))          //行为单位
+	//	{
+	//		vec.push_back(buf);
+	//	}*/
+
+	//	while (input >> buf)               //空格为间隔
+	//	{
+	//		vec.push_back(buf);
+	//	}
+	//}
+	//else
+	//{
+	//	cerr << "can not open this file" << fileName << endl;
+	//}
+	//
+	//cout << vec[1] << endl;
+
+	//PrintArgv(argc,argv);
+	
+	/*input.open("x.txt");
+	if (input)
+	{
+		input.getline
+	}*/
+
+	//if (input)
+	//{
+	//	myStreamLearn(input);
+	//	cout << "success";
+	//}
+	//else
+	//{
+	//	cout << "fail";
+	//	myStreamLearn(cin);
+	//	
+	//	//cout << endl;
+	//	
+	//}
+
+	/*string line, word;
+	vector<PersonInfo> people;
+	while (getline(cin, line))
+	{
+		PersonInfo info;
+		istringstream record(line);
+		record >> info.name;
+		while (record >> word)
+		{
+			info.phones.push_back(word);
+		}
+
+		people.push_back(info);
+
+	}*/
 
 	return 0;
 }
